af5d4e02af4c33353f247361d90d0294d46b2ef8
ctacuri <cesar_hbk@hotmail.com>
2017-06-10 00:09:40 -0500

    correccion connect Mysql en Bitnami

--- connection/connMYSQL.js
+++ connection/connMYSQL.js
@@ -15,6 +15,7 @@ var mysql = require("mysql"),
 		database: "gim_escuela"
 	};
 
+/*
 function fnConectado(err){
 	if(err){
 		console.log("Error = " + err);	
@@ -25,5 +26,31 @@ function fnConectado(err){
 
 var conexion = mysql.createConnection(opciones);
 conexion.connect(fnConectado);
+*/
 
-module.exports = conexion;
+var connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(opciones); 		// Recreate the connection, since
+                                                    		// the old one cannot be reused.
+    connection.connect(function(err) {              		// The server is either down
+        if(err) {                                     			// or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }else{
+            console.log("MySQL conectado en el PID = " + connection.threadId);
+        }                                    // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+module.exports = connection;
